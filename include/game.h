816e0ec2c8865ebf19200cb19c47f89f22a9ac18
nati-sahlu <natansahlu5@gmai.com>
2025-03-14 17:41:50 +0300

    started

--- include/game.h
+++ include/game.h
@@ -10,6 +10,12 @@ extern SDL_Window *window;
 extern SDL_Renderer *renderer;
 extern int isRunning;
 
+// Map dimensions
+#define MAP_WIDTH 8
+#define MAP_HEIGHT 8
+
+// Maze layout (1 = wall, 0 = empty space)
+extern int maze[MAP_HEIGHT][MAP_WIDTH];
 int initSDL(void);
 void gameLoop(void);
 void cleanup(void);
