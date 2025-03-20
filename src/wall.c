1d0740d892c4b4f96e00ee8108c4d2998c0a4a0b
AbdulmelikShemsedin <abdulmelikshemsedin@gmail.com>
2025-03-20 21:20:08 +0300

    add wall

---
+++ src/wall.c
@@ -0,0 +1,44 @@
+#include "../include/header.h"
+
+/**
+ * render_wall - Renders walls using simple shading instead of textures.
+ * This function calculates the correct height and position for each
+ * vertical wall slice based on the ray distances.
+ */
+void render_wall(void)
+{
+        int x, y, wall_bottom_pixel, wall_strip_height, wall_top_pixel;
+        float perp_distance, projected_wall_height;
+        color_t wall_color;
+
+        for (x = 0; x < NUM_RAYS; x++)
+        {
+                perp_distance = rays[x].distance * cos(rays[x].ray_angle - player.rotation_angle);
+
+
+                projected_wall_height = (TILE_SIZE / perp_distance) * PROJ_PLANE;
+                wall_strip_height = (int)projected_wall_height;
+
+
+                wall_top_pixel = (SCREEN_HEIGHT / 2) - (wall_strip_height / 2);
+
+                if (wall_top_pixel < 0)
+                        wall_top_pixel = 0;
+
+                wall_bottom_pixel = (SCREEN_HEIGHT / 2) + (wall_strip_height / 2);
+                if (wall_bottom_pixel > SCREEN_HEIGHT)
+                        wall_bottom_pixel = SCREEN_HEIGHT;
+
+
+                wall_color = 0xAAAAAA;
+
+
+
+                for (y = wall_top_pixel; y < wall_bottom_pixel; y++)
+                {
+
+
+                        set_pixel_color(x, y, wall_color);
+                }
+        }
+}
